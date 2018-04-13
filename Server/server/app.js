813cde986a55ecd6f2d04ab72c7cf5a9f43cd6c7
Ashley Louitan <jataveius@aol.com>
2018-04-12 23:21:21 -0400

    updated mongoDB coding

--- Server/server/app.js
+++ Server/server/app.js
@@ -64,32 +64,41 @@ app.use('/user', user);
 
 // Mongo Connection //
 
+
+// Set up promises with mongoose
 mongoose.Promise = global.Promise;
-var mongoURI = '';
-// process.env.MONGODB_URI will only be defined if you
-// are running on Heroku
-if(process.env.MONGODB_URI != undefined) {
-    // use the string value of the environment variable
-    mongoURI = process.env.MONGODB_URI;
-} else {
-    // use the local database server
-    mongoURI = 'mongodb://localhost:27017/chorescheckupapp';
-}
+// Connect to the Mongo DB
+//add mongo heroku uri
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/chorescheckupapp"
+);
+
+
+// var mongoURI = '';
+// // process.env.MONGODB_URI will only be defined if you
+// // are running on Heroku
+// if(process.env.MONGODB_URI != undefined) {
+//     // use the string value of the environment variable
+//     mongoURI = process.env.MONGODB_URI;
+// } else {
+//     // use the local database server
+//     mongoURI = 'mongodb://localhost:27017/chorescheckupapp';
+// }
 
 // var mongoURI = "mongodb://localhost:27017/passport";
-var mongoDB = mongoose.connect(mongoURI).connection;
+// var mongoDB = mongoose.connect(mongoURI).connection;
 
 
-mongoDB.on('error', function(err){
-   if(err) {
-     console.log("MONGO ERROR: ", err);
-   }
-   res.sendStatus(500);
-});
+// mongoDB.on('error', function(err){
+//    if(err) {
+//      console.log("MONGO ERROR: ", err);
+//    }
+//    res.sendStatus(500);
+// });
 
-mongoDB.once('open', function(){
-   console.log("Connected to Mongo!");
-});
+// mongoDB.once('open', function(){
+//    console.log("Connected to Mongo!");
+// });
 
 // App Set //
 app.set('port', (process.env.PORT || 3030));
